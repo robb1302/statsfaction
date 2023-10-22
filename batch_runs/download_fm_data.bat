e0ee596dbd5eb54d27412c8094555a686c11b212
Robert Gerth <robert.gerth@mail.de>
2023-10-22 17:17:54 +0200

    update players

--- batch_runs/download_fm_data.bat
+++ batch_runs/download_fm_data.bat
@@ -10,8 +10,9 @@ call "%VENV_PATH%\Scripts\activate"
 rem Führen Sie Ihren Befehl innerhalb der virtuellen Umgebung aus
 rem Zum Beispiel, Python-Skript ausführen:
 
-py .\scripts\sport_analytics\run_crawler_fm_players.py --database_version 18
-py .\scripts\sport_analytics\run_crawler_fm_attributes.py --database_version 18
+py .\scripts\sport_analytics\run_crawler_fm_attributes.py --database_version  "FM 22 (22.1.0)"
+py .\scripts\sport_analytics\run_crawler_fm_attributes.py --database_version "FM 23 (23.4.0)"
+
 
 rem Deaktivieren Sie die virtuelle Umgebung, wenn Sie fertig sind
 call "%VENV_PATH%\Scripts\deactivate"
