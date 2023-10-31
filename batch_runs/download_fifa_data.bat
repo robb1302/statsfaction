4f073dff1d9051473f8845585249062a4648957d
Robert Gerth <robert.gerth@mail.de>
2023-10-31 22:41:57 +0100

    restructure training

--- batch_runs/download_fifa_data.bat
+++ batch_runs/download_fifa_data.bat
@@ -10,9 +10,9 @@ call "%VENV_PATH%\Scripts\activate"
 rem Führen Sie Ihren Befehl innerhalb der virtuellen Umgebung aus
 rem Zum Beispiel, Python-Skript ausführen:
 
-py .\scripts\sport_analytics\run_crawler_fifa_players.py --fifa_versions 18
-py .\scripts\sport_analytics\run_crawler_fifa_attributes.py --fifa_versions 18
-py .\scripts\sport_analytics\merge_crawled_fifa_data.py --fifa_versions 18
+py .\scripts\sport_analytics\run_crawler_fifa_players.py --fifa_versions 12,11
+py .\scripts\sport_analytics\run_crawler_fifa_attributes.py --fifa_versions 12,11
+py .\scripts\sport_analytics\merge_crawled_fifa_data.py --fifa_versions 12,11
 
 rem Deaktivieren Sie die virtuelle Umgebung, wenn Sie fertig sind
 call "%VENV_PATH%\Scripts\deactivate"
